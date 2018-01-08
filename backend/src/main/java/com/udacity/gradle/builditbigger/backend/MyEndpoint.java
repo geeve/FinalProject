9cd282f9e34b84b45e54e41851a888f8857e647c
geeve <geeve@126.com>
2018-01-08 21:42:30 +0800

    Step 3:Create GCE model.

--- backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
+++ backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
@@ -6,6 +6,7 @@
 
 package com.udacity.gradle.builditbigger.backend;
 
+import com.example.JokeProvider;
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
@@ -26,9 +27,10 @@ public class MyEndpoint {
 
     /** A simple endpoint method that takes a name and says Hi back */
     @ApiMethod(name = "sayHi")
-    public MyBean sayHi(@Named("name") String name) {
+    public MyBean sayHi(@Named("name") int index) {
         MyBean response = new MyBean();
-        response.setData("Hi, " + name);
+
+        response.setData(JokeProvider.getJoke(index));
 
         return response;
     }
